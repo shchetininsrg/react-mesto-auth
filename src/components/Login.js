cf41a3bfab51ee3c968d5fc1c519efc5e5923a81
Sergey Shchetinin <shchetinin.srg@yandex.ru>
2023-12-08 21:15:28 +0500

    final

--- src/components/Login.js
+++ src/components/Login.js
@@ -16,29 +16,31 @@ export default function Login({ onLogin }) {
     onLogin(email, password);
   }
   return (
-    <div className="auth">
-      <h2 className="auth__title">Вход</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          className="auth__input"
-          type="email"
-          name="email"
-          placeholder="Email"
-          value={email}
-          onChange={handleChangeEmail}
-        />
-        <input
-          className="auth__input"
-          type="password"
-          name="password"
-          placeholder="Пароль"
-          value={password}
-          onChange={handleChangePassword}
-        />
-        <button className="auth__button" type="submit">
-          Войти
-        </button>
-      </form>
-    </div>
+    <main>
+      <div className="auth">
+        <h2 className="auth__title">Вход</h2>
+        <form onSubmit={handleSubmit}>
+          <input
+            className="auth__input"
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={email}
+            onChange={handleChangeEmail}
+          />
+          <input
+            className="auth__input"
+            type="password"
+            name="password"
+            placeholder="Пароль"
+            value={password}
+            onChange={handleChangePassword}
+          />
+          <button className="auth__button" type="submit">
+            Войти
+          </button>
+        </form>
+      </div>
+    </main>
   );
 }
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import React from "react";
+import Main from "./Main";
 
 export default function Register({ onRegister }) {
   const [email, setEmail] = React.useState("");
@@ -18,35 +19,37 @@ export default function Register({ onRegister }) {
   }
 
   return (
-    <div className="auth">
-      <h2 className="auth__title">Регистрация</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          className="auth__input"
-          type="email"
-          name="email"
-          placeholder="Email"
-          value={email}
-          onChange={handleChangeEmail}
-        />
-        <input
-          className="auth__input"
-          type="password"
-          name="password"
-          placeholder="Пароль"
-          value={password}
-          onChange={handleChangePassword}
-        />
-        <button className="auth__button" type="submit">
-          Зарегистрироваться
-        </button>
-        <p className="auth__text">
-          Уже зарегистрировались?{" "}
-          <Link className="auth__link" to="/sign-in">
-            Войти
-          </Link>
-        </p>
-      </form>
-    </div>
+    <main>
+      <div className="auth">
+        <h2 className="auth__title">Регистрация</h2>
+        <form onSubmit={handleSubmit}>
+          <input
+            className="auth__input"
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={email}
+            onChange={handleChangeEmail}
+          />
+          <input
+            className="auth__input"
+            type="password"
+            name="password"
+            placeholder="Пароль"
+            value={password}
+            onChange={handleChangePassword}
+          />
+          <button className="auth__button" type="submit">
+            Зарегистрироваться
+          </button>
+          <p className="auth__text">
+            Уже зарегистрировались?{" "}
+            <Link className="auth__link" to="/sign-in">
+              Войти
+            </Link>
+          </p>
+        </form>
+      </div>
+    </main>
   );
 }
